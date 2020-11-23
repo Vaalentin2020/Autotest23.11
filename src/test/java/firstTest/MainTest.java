554c906ec972282f501ba81aa02c69dd523457ff
valentin <brandstore161@yandex.ru >
2020-11-23 19:58:14 +0300

    Commit 1

--- src/test/java/firstTest/MainTest.java
+++ src/test/java/firstTest/MainTest.java
@@ -1,14 +1,21 @@
 package firstTest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
     String login = "supertester";
     String password = "777";
-    Automation automation = new Automation();
+    LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
     @Test
     public void loginTest(){
-        automation.testLogin(login, password);
+        loginPage.testLogin(login, password);
+    }
+    @Test
+    public void checkHomePageUi(){
+        homePage.helpButton(login, password);
     }
 }
 
